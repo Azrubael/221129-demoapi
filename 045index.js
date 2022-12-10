62340174206c5dc2c0b14726a493e5cb8517b85f
Azrubael <i0638464000@gmail.com>
2022-12-10 19:10:58 +0200

    Lesson #047. Options with middleware

--- 045index.js
+++ 045index.js
@@ -3,10 +3,25 @@ import express from 'express'
 const port = 9001
 const app = express()
 
-app.get('/hello', (req, res) => {
+app.all('/hel?lo', (req, res, next) => {
+   // '?' говорит об опциональности одного символа
+   // '*' говорит о наличии любого символа
+   // '+' говорит о наличии любого числа указанных символов
+   // '(ell)?' для необязательной группы символов
+   // '/.*a$/' regexp, где $ означает окончание строки
+   console.log('All')
+   next()
+})
+
+const callBack = (req, res, next) => {
+   console.log('CallBack')
+   next()
+}
+
+app.get('/hello', callBack, (req, res) => {
    res.send('Привет, Azrubael !!!')
 })
 
 app.listen(port, () => {
-    console.log('Сервер запущен на http://localhost:' + port)
+   console.log('Сервер запущен на http://localhost:' + port)
 })
