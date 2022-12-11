e10c0aaf8cc1f72e86f3ee2f3e1c181cac3d2f93
Azrubael <i0638464000@gmail.com>
2022-12-11 09:41:19 +0200

    Lesson #049. Separate module './users/users.js' for handling routes

--- 045index.js
+++ 045index.js
@@ -1,7 +1,13 @@
 import express from 'express'
+import { userRouter } from './users/users.js'
 
 const port = 9001
 const app = express()
+
+app.use((req, res, next) => {
+   console.log('Current time is ', Date.now())
+   next()
+})
 
 app.all('/hel?lo', (req, res, next) => {
    // '?' говорит об опциональности одного символа
@@ -19,8 +25,11 @@ const callBack = (req, res, next) => {
 }
 
 app.get('/hello', callBack, (req, res) => {
+   res.type('application/json')
    res.send('Привет, Azrubael !!!')
 })
+
+app.use('/users', userRouter)
 
 app.listen(port, () => {
    console.log('Сервер запущен на http://localhost:' + port)
