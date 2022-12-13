801b4784b45890d4417723b4bed66859fa01ac11
Azrubael <i0638464000@gmail.com>
2022-12-13 13:30:19 +0200

    Lesson #064. Logger as a simple dependency injecyion

--- src/063main.ts
+++ src/063main.ts
@@ -1,9 +1,11 @@
 // это будет основная точка входа для запуска приложения
 import { App } from "./app";
+import { LoggerService } from "./logger/logger-service";
 
 
 async function bootstrap() {
-   const app = new App()
+   // dependency ingection
+   const app = new App(new LoggerService())
    await app.init()
 }
 
--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,21 @@
 import express, { Express } from 'express'
 import { userRouter } from './users/users'
 import { Server } from 'http'
+import { LoggerService } from './logger/logger-service'
 
 export class App {
    app: Express
    server: Server
    port: number
+   logger: LoggerService
 
-   constructor() {
+   constructor(logger: LoggerService) {
       this.app = express()
       this.port = 9001
+      this.logger = logger
    }
 
-   // метод, дополняющий наше приложение
+   // метод, дополняющий наше приложение маршрутом
    useRoutes() {
       this.app.use('/users', userRouter)
 
@@ -22,7 +25,7 @@ export class App {
    public async init() {
       this.useRoutes()
       this.server = this.app.listen(this.port)
-      console.log('Сервер запущен на http://localhost:' + this.port)
+      this.logger.log('Сервер запущен на http://localhost:' + this.port)
 
    }
 }
