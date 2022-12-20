ac40a6bd3dcdcaa163b513a255d51f88f3076fd2
Azrubael <i0638464000@gmail.com>
2022-12-20 12:28:15 +0200

    Lesson #083. Data validation, well tuned and checked

--- src/users/users-controller.ts
+++ src/users/users-controller.ts
@@ -8,19 +8,24 @@ import { TYPES } from '../types'
 import { IUserController } from './users-controller-interface'
 import { UserLoginDto } from './dto/user-login.dto'
 import { UserRegisterDto } from './dto/user-register.dto'
-import { UserEntity } from './user-entity'
-import { UserService } from './users-service'
+import { ValidateMiddleware } from '../common/validate-middleware'
+import { IUserService } from './users-service-interface'
 
 @injectable()
 export class UserController extends BaseController implements IUserController {
 	constructor(
 		@inject(TYPES.ILogger) private loggerService: ILogger,
-		@inject(TYPES.UserService) private userService: UserService
+		@inject(TYPES.UserService) private userService: IUserService
 	) {
 		super(loggerService)
 		this.bindRoutes([
 			{ path: '/login', method: 'post', func: this.login },
-			{ path: '/register', method: 'post', func: this.register },
+			{
+				path: '/register',
+				method: 'post',
+				func: this.register,
+				middlewares: [new ValidateMiddleware(UserRegisterDto)],
+			},
 		])
 	}
 
